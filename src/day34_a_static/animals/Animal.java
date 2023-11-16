67e1613c8bba84adbefc1d4cf8b82e6ef73b1e56
Gular <ghebibova@gmail.com>
2023-11-16 15:51:00 -0500

    My new commit  day_34 static class

---
+++ src/day34_a_static/animals/Animal.java
@@ -0,0 +1,11 @@
+package day34_a_static.animals;
+
+public class Animal {
+    String species;
+
+    int numberOfLegs;
+
+    public void walk () {
+        System.out.println(species + " is walking");
+    }
+}
